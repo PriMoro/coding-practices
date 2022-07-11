9a4ebe1c0aa19549b3bb7268f8eae996b1da2fe7
PriMoro <86128822+PriMoro@users.noreply.github.com>
2022-07-10 21:12:25 -0300

    create repo

---
+++ day1.js
@@ -0,0 +1,36 @@
+function vowelsAndConsonants(s) {
+  for (let i = 0; i < s.length; i++) {
+    if (
+      s[i] === "a" ||
+      s[i] === "e" ||
+      s[i] === "i" ||
+      s[i] === "o" ||
+      s[i] === "u"
+    ) {
+      console.log(s[i]);
+    }
+  }
+  for (let i = 0; i < s.length; i++) {
+    if (
+      s[i] != "a" &&
+      s[i] != "e" &&
+      s[i] != "i" &&
+      s[i] != "o" &&
+      s[i] != "u"
+    ) {
+      console.log(s[i]);
+    }
+  }
+  //   for (v of s) {
+  //     if (v === "a" || v === "e" || v === "i" || v === "o" || v === "u") {
+  //       console.log(v);
+  //     }
+  //   }
+  //   for (v of s) {
+  //     if (v != "a" && v != "e" && v != "i" && v != "o" && v != "u") {
+  //       console.log(v);
+  //     }
+  //   }
+}
+let s = "learnjavascriptloopsinhackerrankweekofcode";
+vowelsAndConsonants(s);
