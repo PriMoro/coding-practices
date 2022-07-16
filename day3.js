cc9ed40c153ed2a66fb0224deb01423a9ff2e70d
PriMoro <86128822+PriMoro@users.noreply.github.com>
2022-07-16 17:17:23 -0300

    finished-day3

--- day3.js
+++ day3.js
@@ -1,13 +1,10 @@
 function reverse(s) {
   try {
-    s.split("").reverse().join("");
-    console.log(s);
+    s = s.split("").reverse().join("");
+    //console.log(s);
   } catch (error) {
     console.log(error);
   } finally {
-    // if (error) {
-    //   console.log(error);
-    // }
     console.log(s);
   }
 }
