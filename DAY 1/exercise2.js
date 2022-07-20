8414e1b867f9802d346056dc6e6e06b73d6d4ed0
PriMoro <86128822+PriMoro@users.noreply.github.com>
2022-07-20 19:37:57 -0300

    testing fatctorial exercise

--- DAY 1/exercise2.js
+++ DAY 1/exercise2.js
@@ -4,3 +4,8 @@ function factorial(n) {
   }
   return n * factorial(n - 1);
 }
+console.log(factorial(4)); //24
+console.log(factorial(5)); //120
+console.log(factorial(6)); //720
+console.log(factorial(10)); //3628800
+console.log(factorial(15)); //1307674368000
