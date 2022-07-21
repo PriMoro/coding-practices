f484620b2fcbeb72ad31f899037416f3479ea787
PriMoro <86128822+PriMoro@users.noreply.github.com>
2022-07-20 22:22:46 -0300

    modify array by even and odd

--- DAY 5/exercise1.js
+++ DAY 5/exercise1.js
@@ -1,1 +1,11 @@
-//EXERCISE 1
+function modifyArray(nums) {
+  let count = 0;
+  while (count < nums.length) {
+    nums[count] % 2 === 0
+      ? (nums[count] = nums[count] * 2)
+      : (nums[count] = nums[count] * 3);
+    count++;
+  }
+  return nums;
+}
+console.log(modifyArray([1, 2, 3, 4, 5]));
