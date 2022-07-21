901ee74b3313b820d38da13a16212ef68bb38b21
PriMoro <86128822+PriMoro@users.noreply.github.com>
2022-07-20 22:31:27 -0300

    improve code

--- DAY 5/exercise1.js
+++ DAY 5/exercise1.js
@@ -3,9 +3,7 @@
 function modifyArray(nums) {
   let count = 0;
   while (count < nums.length) {
-    nums[count] % 2 === 0
-      ? (nums[count] = nums[count] * 2)
-      : (nums[count] = nums[count] * 3);
+    nums[count] % 2 === 0 ? (nums[count] *= 2) : (nums[count] *= 3);
     count++;
   }
   return nums;
