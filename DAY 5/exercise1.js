d9056fb546fb0dcd8a29a77a2c906c2c240d778b
PriMoro <86128822+PriMoro@users.noreply.github.com>
2022-07-20 22:32:27 -0300

    other way to solve

--- DAY 5/exercise1.js
+++ DAY 5/exercise1.js
@@ -1,11 +1,20 @@
 //EXERCISE 1
 
 function modifyArray(nums) {
-  let count = 0;
-  while (count < nums.length) {
-    nums[count] % 2 === 0 ? (nums[count] *= 2) : (nums[count] *= 3);
-    count++;
+  for (n in nums) {
+    nums[n] % 2 === 0 ? (nums[n] *= 2) : (nums[n] *= 3);
   }
   return nums;
 }
 console.log(modifyArray([1, 2, 3, 4, 5])); //[ 3, 4, 9, 8, 15 ]
+
+// OTHER WAY TO SOLVE
+// function modifyArray(nums) {
+//   let count = 0;
+//   while (count < nums.length) {
+//     nums[count] % 2 === 0 ? (nums[count] *= 2) : (nums[count] *= 3);
+//     count++;
+//   }
+//   return nums;
+// }
+// console.log(modifyArray([1, 2, 3, 4, 5])); //[ 3, 4, 9, 8, 15 ]
