cf2904782e2203cda7eb73d16dcbf59a17e535cb
PriMoro <86128822+PriMoro@users.noreply.github.com>
2022-07-23 16:25:53 -0300

    classes and prototypes

--- DAY 5/exercise2.js
+++ DAY 5/exercise2.js
@@ -1,1 +1,30 @@
 // EXERCISE 2
+/*
+
+Task
+
+We provide the implementation for a Rectangle class in the editor. Perform the following tasks:
+
+Add an area method to Rectangle's prototype.
+Create a Square class that satisfies the following:
+It is a subclass of Rectangle.
+It contains a constructor and no other methods.
+It can use the Rectangle class' area method to print the area of a Square object.
+Locked code in the editor tests the class and method implementations and prints the area values to STDOUT.
+
+*/
+
+class Rectangle {
+  constructor(w, h) {
+    this.w = w;
+    this.h = h;
+  }
+  get area() {
+    return this.w * this.h;
+  }
+}
+class Square extends Rectangle {
+  constructor(props) {
+    super(props, props);
+  }
+}
